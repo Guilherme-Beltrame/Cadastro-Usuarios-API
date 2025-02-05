3c4cbe4fdd414c5b7d8e45b4d2c0b522c9e3c53f
Guilherme-Beltrame <gbeltramem@gmail.com>
2025-02-05 15:53:53 -0300

    Atualizando Metodos

--- Server.js
+++ Server.js
@@ -1,23 +1,68 @@
 import express from "express";
-
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 const app = express();
+
+// Define que usaremos json como metodo de adicionar post
+app.use(express.json());
 
 //Porta do computador que vai rodar
 app.listen(3000);
 
-//pega os usuarios
-app.get("/usuarios", (req, res) =>
-	res.send("Ok, deu muito bom para primeira tentativa"),
-);
-
 // Cria/adiciona usuarios
-app.post("/usuarios");
+app.post("/usuarios", async (req, res) => {
+	await prisma.User.create({
+		data: {
+			email: req.body.email,
+			name: req.body.nome,
+			agr: req.body.idade,
+		},
+	});
+	res.status(201).json(req.body);
+});
+
+//pega os usuarios
+app.get("/usuarios", async (req, res) => {
+	let users = []
+	if (req.query) {
+		users = await prisma.user.findMany({
+			where: {
+				name: req.query.name,
+				age: req.query.age,
+				email: req.query.email
+			},
+		});
+	} else {
+		users = await prisma.user.findMany();
+	}
+
+	res.status(200).json(users);
+});
 
 // Edita varios usuarios
-app.put("/usuarios");
+app.put("/usuarios/:id", async (req, res) => {
+	await prisma.User.update({
+		where: {
+			id: req.params.id,
+		},
+		data: {
+			email: req.body.email,
+			name: req.body.nome,
+			agr: req.body.idade,
+		},
+	});
+	res.status(201).json(req.body);
+});
 
 // Deleta Usuarios
-app.delete("/usuarios");
+app.delete("/usuarios/:id", async (req, res) => {
+	await prisma.User.delete({
+		where: {
+			id: req.params.id,
+		},
+	});
+	res.status(200).json({ Message: "Usuario excluido com sucesso!" });
+});
 
 /* As rotas precisam de duas coisas:
     1) Tipo da rota, o metodo http que vai usar (get, post, delete ou put).
